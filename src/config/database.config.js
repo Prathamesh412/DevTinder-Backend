c009478be14c29e3e86b2cb4a8598bafec3c1fb0
Prathamesh Prabhu <prabhu@Prachitis-MacBook-Air.local>
2025-12-10 23:54:25 +0530

    database deployment

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+MONGODB_URI = `mongodb+srv://prathprabhu:wpAf2epXKnD4QNyo@mumbai.rgnouqh.mongodb.net/NamasteNode`
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(MONGODB_URI)
 }
 
 module.exports = connectDB;
